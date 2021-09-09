41b698eccb88148463ac1a6f19b5d6de48187945
elsafir <49156274+elsafir@users.noreply.github.com>
2021-09-09 20:22:49 +0700

    alert_dialog

--- Kelompok/app/src/main/java/com/ppb/kelompok/MainActivity.java
+++ Kelompok/app/src/main/java/com/ppb/kelompok/MainActivity.java
@@ -1,15 +1,20 @@
 package com.ppb.kelompok;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    
+
     private CheckBox cbEdu, cbLife, cbTech;
     private Button btSubmit;
 
@@ -21,6 +26,42 @@ public class MainActivity extends AppCompatActivity {
         setCheckBoxListener();
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu1:
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        this);
+                alertDialogBuilder.setTitle("Keluar dari aplikasi?");
+                alertDialogBuilder
+                        .setMessage("Klik Ya untuk keluar!")
+                        .setIcon(R.mipmap.ic_launcher)
+                        .setCancelable(false)
+                        .setPositiveButton("Ya",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                MainActivity.this.finish();
+                            }
+                        })
+                        .setNegativeButton("Tidak",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                // membuat alert dialog dari builder
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // menampilkan alert dialog
+                alertDialog.show();
+        }
+                return true;
+        }
 
     private void setCheckBoxListener() {
         cbEdu = (CheckBox) findViewById(R.id.cb_education);

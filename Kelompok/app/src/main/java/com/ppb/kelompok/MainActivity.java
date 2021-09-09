8c5bd24f372543edff5fa01fe69cdd7ced25a37b
elsafir <49156274+elsafir@users.noreply.github.com>
2021-09-09 19:53:41 +0700

    toast dan checkbox

--- Kelompok/app/src/main/java/com/ppb/kelompok/MainActivity.java
+++ Kelompok/app/src/main/java/com/ppb/kelompok/MainActivity.java
@@ -1,14 +1,50 @@
 package com.ppb.kelompok;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    
+    private CheckBox cbEdu, cbLife, cbTech;
+    private Button btSubmit;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setCheckBoxListener();
+
     }
-}
+
+    private void setCheckBoxListener() {
+        cbEdu = (CheckBox) findViewById(R.id.cb_education);
+        cbLife = (CheckBox) findViewById(R.id.cb_lifestyle);
+        cbTech = (CheckBox) findViewById(R.id.cb_technology);
+        cbEdu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (((CheckBox) view).isChecked()) {
+                    Toast.makeText(MainActivity.this,
+                            "Anda memilih Education", Toast.LENGTH_LONG).show(); }
+            }});
+        cbLife.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (((CheckBox) view).isChecked()) {
+                            Toast.makeText(MainActivity.this,
+                                    "Anda memilih Lifestyle", Toast.LENGTH_LONG).show();
+                        }
+            }
+        });
+        cbTech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (((CheckBox) view).isChecked()) {
+                    Toast.makeText(MainActivity.this,
+                            "Anda memilih Technology", Toast.LENGTH_LONG).show();}}});}}
